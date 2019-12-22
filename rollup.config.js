79cc8195bb0137712ce67b475b2d9c5fd17eec68
Jakub Biesiada <biesiadajakub@icloud.com>
2019-12-23 00:22:33 +0100

    Update

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
