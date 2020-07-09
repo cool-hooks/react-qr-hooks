099076ab7ee6c07d7c58006c2404403e0430f767
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-09 21:58:01 +0200

    chore: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: ['prettier', 'react-hooks'],
   env: {
     browser: true,
+    node: true,
     jest: true,
   },
   parserOptions: {
--- __tests__/useQr.spec.ts
+++ __tests__/useQr.spec.ts
@@ -2,8 +2,24 @@ import { renderHook } from '@testing-library/react-hooks';
 
 import { useQrEncode, useQrDecode } from '../src';
 
+jest.mock('qrcode', () => ({
+  toDataURL: (..._: any) => {
+    return new Promise((resolve, reject) => {
+      resolve('');
+    });
+  },
+}));
+
+jest.mock('qrcode-decoder', () => ({
+  decodeFromImage: (..._: any) => {
+    return new Promise((resolve, reject) => {
+      resolve('');
+    });
+  },
+}));
+
 describe('useQr', () => {
-  it('', () => {
+  it('should ', () => {
     const input = 'Hello World!';
 
     const { result: encoded } = renderHook(() => useQrEncode(input));
